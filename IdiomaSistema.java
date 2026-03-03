d324c60262c2afadcc6b438405ad65bd9fc2e7e3
cristyanphelipe <cristyanphelipe@gmail.com>
2026-03-03 14:21:47 -0300

    up repo

--- IdiomaSistema.java
+++ IdiomaSistema.java
@@ -4,5 +4,3 @@ public class IdiomaSistema {
         System.out.println("O idioma do sistema é: " + idioma);
     }
 }
-
-nao sei se isso é o que você queria, mas esse código em Java exibe o idioma do sistema usando a propriedade "user.language". Se você tiver alguma dúvida ou precisar de mais informações, por favor, me avise!
